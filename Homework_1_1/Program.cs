7f0bd8e5a11f598db4c218bd62a81883fbcf3b15
Anastasia <Nasti.M@list.ru>
2022-11-27 02:05:43 +0300

    Сохранение

---
+++ Homework_1_1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Pleas, input the first integer number!");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Pleas, input the second integer number!");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 > n2)
+{
+    Console.WriteLine("Первое число является max!");
+}
+else
+{
+    Console.WriteLine("Второе число является max!");
+}
+
+if (n2 > n1)
+{
+    Console.WriteLine("Первое число является min!");
+}
+else
+{
+    Console.WriteLine("Второе число является min!");
+}
