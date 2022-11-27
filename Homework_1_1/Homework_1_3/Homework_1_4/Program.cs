54e726da85d12e6346e80725884b18c662ef29f6
Anastasia <Nasti.M@list.ru>
2022-11-27 05:51:57 +0300

    First

--- Homework_1_1/Homework_1_3/Homework_1_4/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.WriteLine("Enter a positive integer!");
-int num = Convert.ToInt32(Console.ReadLine());
-int n = 0;
-
-while (n <= num)  // Выводит все числа от 1 до введенного числа
-{
-    if (n%2 == 0) Console.Write( n + " " );
-    n ++1;
-}
--- Homework_1_1/Program.cs
+++ Homework_1_1/Program.cs
@@ -1,9 +1,9 @@
-﻿Console.WriteLine("Pleas, input the first integer number!");
+﻿Console.WriteLine("Please, input the first integer number!");
 int n1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Pleas, input the second integer number!");
+Console.WriteLine("Please, input the second integer number!");
 int n2 = Convert.ToInt32(Console.ReadLine());
 
-if (n1 > n2)
+if (n1 >n2)
 {
     Console.WriteLine("Первое число является max!");
 }
@@ -12,7 +12,7 @@ else
     Console.WriteLine("Второе число является max!");
 }
 
-if (n2 > n1)
+if (n2 >n1)
 {
     Console.WriteLine("Первое число является min!");
 }
