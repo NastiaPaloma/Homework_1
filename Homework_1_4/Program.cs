b9245b77576edd77fdf908ad8bdd90c74c5923d9
Anastasia <Nasti.M@list.ru>
2022-11-27 06:00:11 +0300

    Шестое

---
+++ Homework_1_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter a positive integer!");
+int number = Convert.ToInt32(Console.ReadLine());
+int num = 0;
+
+while (num <= number)
+{
+    if (num % 2 == 0) Console.Write(num + " ");
+       num++1;
+    
+}
+Console.WriteLine();
